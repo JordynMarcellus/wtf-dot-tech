e31ff5583b950c6730b2c8a17f8ec6cc906644a3
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-08-13 12:57:35 -0400

    Content update

--- config.js
+++ config.js
@@ -1,38 +1,34 @@
 const config = {
-	"gatsby": {
-		"pathPrefix": "/",
-		"siteUrl": "https://learn.hasura.io",
-		"gaTrackingId": null
-	},
-	"header": {
-		"logo": "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
-		"logoLink": "https://learn.hasura.io",
-		"title": "Gatsby Gitbook Boilerplate",
-		"githubUrl": "https://github.com/hasura/gatsby-gitbook-boilerplate",
-		"helpUrl": "",
-		"tweetText": "",
-		"links": [
-			{ "text": "", "link": ""}
-		],
-	},
-	"sidebar": {
-		"forcedNavOrder": [
-			"/introduction",
-    		"/codeblock"
-		],
-		"links": [
-			{ "text": "Hasura", "link": "https://hasura.io"},
-		],
-		"frontline": false,
-		"ignoreIndex": true,
-	},
-	"siteMetadata": {
-		"title": "Gatsby Gitbook Boilerplate | Hasura",
-		"description": "Documentation built with mdx. Powering learn.hasura.io ",
-		"ogImage": null,
-		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
-		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
-	},
+  gatsby: {
+    pathPrefix: "/",
+    siteUrl: "https://learn.hasura.io",
+    gaTrackingId: null,
+  },
+  header: {
+    logo:
+      "https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/favicon.png",
+    logoLink: "https://learn.hasura.io",
+    title: "WTF.tech",
+    githubUrl: "https://github.com/hasura/gatsby-gitbook-boilerplate",
+    helpUrl: "",
+    tweetText: "",
+    links: [{ text: "", link: "" }],
+  },
+  sidebar: {
+    forcedNavOrder: ["/introduction", "/codeblock"],
+    links: [{ text: "WTF?", link: "/" }],
+    frontline: false,
+    ignoreIndex: true,
+  },
+  siteMetadata: {
+    title: "WTF? Technology buzzword buster",
+    description:
+      "A place for busting technology buzzwords. Editable by you on Github!",
+    ogImage: null,
+    docsLocation:
+      "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
+    favicon: "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg",
+  },
 };
 
 module.exports = config;
